6e55b26365fc11368c96e6cca6d50b37e95f3e23
Junjie Zhou <menging1122@gmail.com>
2025-11-18 14:03:04 +0800

    cuda_devices change

--- configs/train_radgenome/jhcpu7.sh
+++ configs/train_radgenome/jhcpu7.sh
@@ -9,7 +9,7 @@ bf16=True
 
 # Device settings
 # Which GPUs to use (comma-separated IDs). Example: 0,1 uses GPU 0 and 1
-cuda_devices="3,4"  
+cuda_devices="0,1"  
 
 # Torchrun settings
 # A free TCP port that worker processes use to communicate
